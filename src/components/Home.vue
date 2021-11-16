5a24bd36b4530f2c0ab127c6bd9c4973a2117cd6
Linus <linushvenfelt@gmail.com>
2021-11-16 20:08:48 +0100

    Optimizing sampling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,14 @@
 
     <p id="error" v-if="error">{{error}}</p>
 
+    <!--
     <label for="note">Insert a chord to get the relevant scale:</label>
-    <input type="text" name="note" id="note" v-model="note">
+    <input type="text" name="note" id="note" v-model="note"> 
 
     <br><br>
-    <button v-on:click="getData(note)">Get scale</button>
+    <button v-on:click="getData(note)">Get scale</button>-->
+
+    <input type="number" name="bpm" id="bpm" v-model="bpm">
 
     <br><br>
     <ul id="notes" v-if="currentScale">
@@ -18,6 +21,7 @@
 
     <br><br>
     <button v-on:click="play()">PLAY</button>
+    <button v-on:click="stop()">STOP</button>
 
   </div>
 </template>
@@ -32,9 +36,7 @@ const snare = new Tone.Player("https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/exampl
 const hihat =  new Tone.Player("https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/hh.mp3").toDestination();
 //const polySynth = new Tone.PolySynth().toDestination();
 
-Tone.Transport.bpm.value = 120;
 let seq = new Tone.Sequence(function(time, idx) {
-
   hihat.start();
   if ([0, 4, 8, 12].indexOf(idx) >= 0) {
     kick.start();
@@ -43,7 +45,6 @@ let seq = new Tone.Sequence(function(time, idx) {
   if ([2, 6, 10, 14].indexOf(idx) >= 0) {
     snare.start();
   }
-
 }, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], "8n");
 
 export default {
@@ -55,6 +56,7 @@ export default {
         return {
             error: "",
             note: "",
+            bpm: 120,
             currentScale: "",
             sampler: null
         }
@@ -62,14 +64,21 @@ export default {
     methods: {
       prepare: function() {
         console.log("test");
+        const context = new Tone.Context({ latencyHint: "interactive" });
+        Tone.setContext(context);
       },
 
       play: function() {
+        Tone.Transport.bpm.value = this.bpm;
         //this.polySynth.triggerAttackRelease(["C4", "E3", "G3"], "16n");
 
 
         Tone.Transport.start();
         seq.start();
+      },
+      stop: function() {
+        Tone.Transport.stop();
+        seq.stop();
       },
 
 
