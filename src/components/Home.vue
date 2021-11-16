126b94403dba0c6a24517f4692ac9510ef885a35
Linus <linushvenfelt@gmail.com>
2021-11-16 14:11:09 +0100

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>1DV609 - Assignemnt 1 Part 2</h1>
+
+    <p id="error" v-if="error">{{error}}</p>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  created () {
+    this.getPeople()
+  },
+    data: function() {
+        return {
+            error: "",
+        }
+    },
+    methods: {
+
+      getPeople: function() {
+        /*
+          axios.get("http://localhost:3000/users").then((response) => {
+              if (response != null) {
+                  this.people = response.data;
+              } else {
+                  console.log(response);
+              }
+          })*/
+      },
+
+    }
+}
+</script>
---
+++ tests/test.js
@@ -0,0 +1,5 @@
+var assert = require('assert');
+var expect = require('expect.js');
+
+
+//expect(  (document.getElementById("x")).to.be.ok() );
