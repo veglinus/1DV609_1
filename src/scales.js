cdb13204bcc83413efd63c7795a61bcc17f0f0f1
Linus <linushvenfelt@gmail.com>
2021-11-16 18:37:13 +0100

    Added minor scale

--- src/scales.js
+++ src/scales.js
@@ -17,7 +17,21 @@ export default class Scales extends Notes {
         result.push(this.stepWhole()); // G
         result.push(this.stepWhole()); // A
         result.push(this.stepWhole()); // B
+        return result;
+    }
 
+    getMinorScale(input) {
+        let result = [];
+        this.setStartingNote(input);
+
+        // A minor example
+        result.push(input); // A
+        result.push(this.stepWhole()); // B
+        result.push(this.stepHalf()); // C
+        result.push(this.stepWhole()); // D
+        result.push(this.stepWhole()); // E
+        result.push(this.stepHalf()); // F
+        result.push(this.stepWhole()); // A
         return result;
     }
 }
--- tests/unit/testScales.spec.js
+++ tests/unit/testScales.spec.js
@@ -1,12 +1,22 @@
 import { expect } from 'chai'
 import Scales from "../../src/scales.js";
 
-it('Get major scale', () => {
+it('Test major scale', () => {
 
     let scales = new Scales();
 
     let result = scales.getMajorScale("C");
     expect(result).to.have.lengthOf(7);
     expect(result).to.eql(["C", "D", "E", "F", "G", "A", "B"]);
+
+})
+
+it('Test minor scale', () => {
+
+    let scales = new Scales();
+
+    let result = scales.getMinorScale("A");
+    expect(result).to.have.lengthOf(7);
+    expect(result).to.eql(["A", "B", "C", "D", "E", "F", "G"]);
 
 })
