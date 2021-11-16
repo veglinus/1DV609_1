4545eae140fe076f6c48ed48cfb3d8908d2bdb5c
Linus <linushvenfelt@gmail.com>
2021-11-16 18:42:57 +0100

    RED: Tests for pentatonic scale

--- tests/unit/testScales.spec.js
+++ tests/unit/testScales.spec.js
@@ -2,9 +2,7 @@ import { expect } from 'chai'
 import Scales from "../../src/scales.js";
 
 it('Test major scale', () => {
-
     let scales = new Scales();
-
     let result = scales.getMajorScale("C");
     expect(result).to.have.lengthOf(7);
     expect(result).to.eql(["C", "D", "E", "F", "G", "A", "B"]);
@@ -12,11 +10,24 @@ it('Test major scale', () => {
 })
 
 it('Test minor scale', () => {
-
     let scales = new Scales();
-
     let result = scales.getMinorScale("A");
     expect(result).to.have.lengthOf(7);
     expect(result).to.eql(["A", "B", "C", "D", "E", "F", "G"]);
 
+})
+
+it('Test major pentatonic scale', () => {
+    let scales = new Scales();
+    let result = scales.getMajorPentatonicScale("C");
+    expect(result).to.have.lengthOf(5);
+    expect(result).to.eql(["C", "D", "E", "G", "A"]);
+
+})
+
+it('Test minor pentatonic scale', () => {
+    let scales = new Scales();
+    let result = scales.getMinorPentatonicScale("A");
+    expect(result).to.have.lengthOf(5);
+    expect(result).to.eql(["A", "C", "D", "E", "G"]);
 })
