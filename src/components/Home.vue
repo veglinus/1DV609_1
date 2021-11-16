c7a1d7d0561565d114c49dc6064351111aae9fea
Linus <linushvenfelt@gmail.com>
2021-11-16 19:05:37 +0100

    Getting scale in frontend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,16 +7,20 @@
     <label for="note">Insert a chord to get the relevant scale:</label>
     <input type="text" name="note" id="note" v-model="note">
 
+    <br><br>
+    <button v-on:click="getData(note)">Get scale</button>
 
     <br><br>
-    <button v-on:click="test(note)">Get scale</button>
+    <ul id="notes" v-if="currentScale">
+      <li v-for="item in currentScale" :key="item">{{item}}</li>
+    </ul>
 
   </div>
 </template>
 
 <script>
-//import { getScaleStartingPosition } from "../functions.js";
-import Notes from "../../src/notes.js";
+//import Notes from "../../src/notes.js";
+import Scales from "../../src/scales.js";
 
 export default {
 
@@ -24,18 +28,17 @@ export default {
     data: function() {
         return {
             error: "",
-            result: "",
-            note: ""
+            note: "",
+            currentScale: "",
         }
     },
     methods: {
-      test: function(param) {
-        let notes = new Notes();
-        let result = notes.setStartingNote(param);
+      getData: function(input) {
+        let scale = new Scales();
+        let result = scale.getScale(input);
+        this.currentScale = result;
         console.log(result);
-      }
-
-
+      },
     }
 }
 </script>
