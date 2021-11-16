80689234c966fe9ff681755f7725785bdcfb69d4
Linus <linushvenfelt@gmail.com>
2021-11-16 18:45:42 +0100

    GREEN: Major & minor ionic and pentatonic

--- src/scales.js
+++ src/scales.js
@@ -31,6 +31,35 @@ export default class Scales extends Notes {
         result.push(this.stepWhole()); // D
         result.push(this.stepWhole()); // E
         result.push(this.stepHalf()); // F
+        result.push(this.stepWhole()); // G
+        return result;
+    }
+
+    getMinorPentatonicScale(input) {
+        let result = [];
+        this.setStartingNote(input);
+
+        // A minor example
+        result.push(input); // A
+        this.stepWhole(); // B
+        result.push(this.stepHalf()); // C
+        result.push(this.stepWhole()); // D
+        result.push(this.stepWhole()); // E
+        this.stepHalf(); // F
+        result.push(this.stepWhole()); // G
+        return result;
+    }
+
+    getMajorPentatonicScale(input) {
+        let result = [];
+        this.setStartingNote(input);
+
+        // C major example
+        result.push(input); // C
+        result.push(this.stepWhole()); // D
+        result.push(this.stepWhole()); // E
+        this.stepHalf();
+        result.push(this.stepWhole()); // G
         result.push(this.stepWhole()); // A
         return result;
     }
