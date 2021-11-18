11f602151cf319084922a5df0fd71849b6889911
Linus <linushvenfelt@gmail.com>
2021-11-18 17:50:54 +0100

    GREEN: Also refactor of scaleAnalyzer

--- src/classes/scaleAnalyzer.js
+++ src/classes/scaleAnalyzer.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-dupe-class-members */
+
 import Scales from "./scales";
 
 export default class ScaleAnalyzer extends Scales {
@@ -5,7 +7,6 @@ export default class ScaleAnalyzer extends Scales {
         super();
         this.returnvalue = [];
         this.chords;
-
         this.commonChords = [];
     }
 
@@ -14,84 +15,79 @@ export default class ScaleAnalyzer extends Scales {
         this.chords = chords;
 
         chords.forEach(chord => {
-            this.findMajorMinorScales(chord);
+            this.#compare(chord);
         });
-        //console.log("Checking for more scales: " + this.commonChords);
         
-        this.findAllScales(this.commonChords);
+        this.#findAllScales(this.commonChords);
         return this.returnvalue;
     }
 
-    findMajorMinorScales(chord) {
-        let scale = this.getScale(chord);
-        let oldScale = [...scale]; // Change scale degrees into chords
-        if (chord.slice(-1) === "m") {
-            // Minor scale
-            scale[0] = scale[0] + "m";
-            scale[1] = scale[1] + "m";
-            scale[3] = scale[3] + "m";
-            scale[4] = scale[4] + "m";
+    #compare(chord, pentatonic) {
+        let scale;
+        if (pentatonic) {
+            scale = this.getPentatonicScale(chord);
         } else {
-            // Major scale
-            scale[1] = scale[1] + "m";
-            scale[2] = scale[2] + "m";
-            scale[5] = scale[5] + "m";
-            scale[6] = scale[6] + "m";
+            scale = this.getScale(chord);
+        }
+        let oldScale = [...scale];
+        let scaleChords = this.#convertScaleToChords(chord, scale);
+        let matches = this.#computeMatches(scaleChords);
+
+        if (pentatonic) {
+            if (matches >= 2) {
+                this.pushCommonChords(scaleChords);
+                this.returnvalue.push({
+                    name : chord + " pentatonic",
+                    notes : oldScale
+                });
+            }
+        } else {
+            if (matches === this.chords.length) {
+                this.#pushCommonChords(scaleChords);
+                this.returnvalue.push({
+                    name : chord,
+                    notes : oldScale
+                });
+            }
         }
 
-        //console.log(scale);
-        
-        // Check if the array of chords is similar to "chords"
-        let matches = this.computeMatches(scale);
 
-        //console.log("Matches: " + matches);
-
-        if (matches === this.chords.length) {
-            //console.log("Adding " + chord + " scale to result");
-            this.pushCommonChords(scale);
-
-            this.returnvalue.push({
-                name : chord,
-                notes : oldScale
-            });
-        }
     }
 
-    comparePentatonicScale(chord) {
-        let scale = this.getPentatonicScale(chord);
-        let oldScale = [...scale]; // Change scale degrees into chords
+    #convertScaleToChords(chord, scale) {
 
-        if (chord.slice(-1) === "m") {
-            // Minor scale
-            scale[0] = scale[0] + "m";
-            scale[2] = scale[2] + "m";
-            scale[3] = scale[3] + "m";
-        } else {
-            // Major scale
-            scale[1] = scale[1] + "m";
-            scale[2] = scale[2] + "m";
-            scale[4] = scale[4] + "m";
+        if (scale.length === 7) { // Major or minor scale
+            if (chord.slice(-1) === "m") {
+                // Minor scale
+                scale[0] = scale[0] + "m";
+                scale[1] = scale[1] + "m";
+                scale[3] = scale[3] + "m";
+                scale[4] = scale[4] + "m";
+            } else {
+                // Major scale
+                scale[1] = scale[1] + "m";
+                scale[2] = scale[2] + "m";
+                scale[5] = scale[5] + "m";
+                scale[6] = scale[6] + "m";
+            }
+        } else if (scale.length === 5) { // Pentatonic scale
+            if (chord.slice(-1) === "m") {
+                // Minor scale
+                scale[0] = scale[0] + "m";
+                scale[2] = scale[2] + "m";
+                scale[3] = scale[3] + "m";
+            } else {
+                // Major scale
+                scale[1] = scale[1] + "m";
+                scale[2] = scale[2] + "m";
+                scale[4] = scale[4] + "m";
+            }
         }
 
-        console.log(scale);
-        
-        // Check if the array of chords is similar to "chords"
-        let matches = this.computeMatches(scale);
-
-        console.log("Matches: " + matches);
-        // chord === scale[0]
-        if (matches === this.chords.length || matches >= 2) {
-            console.log("Adding " + chord + " pentatonic scale to result");
-            this.pushCommonChords(scale);
-
-            this.returnvalue.push({
-                name : chord + " pentatonic",
-                notes : oldScale
-            });
-        }
+        return scale;
     }
 
-    computeMatches(scale) {
+    #computeMatches(scale) {
         let matches = 0;
         scale.forEach(element => {
             if (this.chords.includes(element)) {
@@ -101,7 +97,7 @@ export default class ScaleAnalyzer extends Scales {
         return matches;
     }
 
-    pushCommonChords(chordArray) {
+    #pushCommonChords(chordArray) {
         chordArray.forEach(chord => {
             if (!this.commonChords.includes(chord)) {
                 this.commonChords.push(chord);
@@ -109,9 +105,9 @@ export default class ScaleAnalyzer extends Scales {
         });
     }
 
-    findAllScales(commonChords) {
+    #findAllScales(commonChords) {
         commonChords.forEach(chord => {
-            this.comparePentatonicScale(chord);
+            this.#compare(chord);
         });
     }
 }
